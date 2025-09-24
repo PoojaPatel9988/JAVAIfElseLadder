ab16e47a97d3e41211937662f09057a97ef1af4d
Pooja Patel <studykhuswaha01@gmail.com>
2025-09-24 21:54:48 +0530

    Add PositiveNegative class

---
+++ src/PositiveNegative.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PositiveNegative {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num;
+        System.out.print("enter any number : ");
+        num = sc.nextInt();
+
+        if (num > 0) {
+            System.out.println("number is positive");
+        } else if (num < 0) {
+            System.out.println("number is negative");
+        } else if (num == 0) {
+            System.out.println("number is zero");
+        } else {
+            System.out.println("invalid number");
+
+        }
+    }
+}
